06e1b2531d5b6a43115f69280ccae2dbab61f889
11-sg <sg4242674@gmail.com>
2024-06-14 00:49:09 +0530

    file added

---
+++ converter.js
@@ -0,0 +1,10 @@
+const baseurl="https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/inr.json";
+
+const dropdown =document.querySelectorAll(".dropdown select");
+
+
+// for(let code in countryList)
+//     {
+//         console.log(code, countryList[code]);
+//     }
+
