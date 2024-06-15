7da1dbde44470a0daa63d4901cb2ef5209f7bd34
11-sg <sg4242674@gmail.com>
2024-06-15 14:19:14 +0530

    update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Currency-Converter
+A simple currency converter application using Javascript.<br>
+This application allows users to convert USD as a base case to currencies. The conversion rates are fetched from the ExchangeRate-API <br>
--- converter.js
+++ converter.js
@@ -1,10 +1,57 @@
-const baseurl="https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/inr.json";
+const baseurl="https://api.currencyapi.com/v3/latest?apikey=cur_live_cbNKHXYSNJ3QhRE8jDsxSmK2ki4jOSU40WRhTYxj";
 
 const dropdown =document.querySelectorAll(".dropdown select");
+const btn =document.querySelector("form button");
+const fromcurr="USD"; // base currency
+const tocurr = document.querySelector(".to select"); 
+const msg = document.querySelector(".msg");
 
+for (let select of dropdown) {
+    for( let currcode in countryList){
+        let newOption = document.createElement("option");
+        newOption.innerText = currcode;
+        newOption.value=currcode;
+         if(select.name === "to" && currcode === "INR")
+                {
+                    newOption.selected = "selected";
+                }
+        select.append(newOption);
+    }
 
-// for(let code in countryList)
-//     {
-//         console.log(code, countryList[code]);
-//     }
+    select.addEventListener("change" , (evt) =>{
+        updateflag(evt.target);
+    }); 
+}
 
+const updateflag = (element) => {
+    let currcode =element.value;
+    let countrycode=countryList[currcode]; // IN ER
+    let newsrc = `https://flagsapi.com/${countrycode}/flat/64.png`;
+    let img = element.parentElement.querySelector("img");
+    img.src=newsrc;
+};
+
+btn.addEventListener("click" ,async (evt) => {
+    evt.preventDefault();
+    let amount=document.querySelector(".amount input");
+    let amtval =amount.value;
+    console.log(amtval);
+    if(amtval === "" || amtval < 1) {
+        amtval = 1;
+        amount.value = "1";
+    }
+
+ let response =await fetch(baseurl);
+ let data =await response.json();  //INR 
+ let finalamount="";
+for(let key of Object.keys(data["data"]))  // to calculate the final amount after changing the currency
+    {  
+        if(tocurr.value === key)
+        { 
+        finalamount =amtval*data["data"][key]["value"];
+        console.log("converted amount",finalamount);
+        break;
+        }
+    }
+    msg.innerText = `${amtval} ${fromcurr} = ${finalamount} ${tocurr.value}`;
+})
